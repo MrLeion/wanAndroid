5f9384e523dd1a610a3419b0d021a6af523897c6
John1211 <shtangzenglei@163.com>
2018-09-26 17:15:07 +0800

    加入publishState 判断

--- java/src/main/java/tzl/com/java/ThreadPool/overschool/OverSchoolByCyclicBarrier.java
+++ java/src/main/java/tzl/com/java/ThreadPool/overschool/OverSchoolByCyclicBarrier.java
@@ -34,7 +34,6 @@ public class OverSchoolByCyclicBarrier {
                  public void run() {
                      try {
                          Thread.sleep(1000);
-
                      } catch (InterruptedException e) {
                          e.printStackTrace();
                      }
